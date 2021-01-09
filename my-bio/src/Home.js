a91ca3a1487f73257df8322abc8d27189b58d439
SitaRamSai <57385355+SitaRamSai@users.noreply.github.com>
2021-01-09 07:43:25 -0600

    Added google auth signin

--- my-bio/src/Home.js
+++ my-bio/src/Home.js
@@ -1,21 +1,35 @@
 import React, { Component } from "react";
- 
+
 class Home extends Component {
   render() {
     return (
       <div>
         <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
- 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+        <p>
+          Cras facilisis urna ornare ex volutpat, et convallis erat elementum.
+          Ut aliquam, ipsum vitae gravida suscipit, metus dui bibendum est, eget
+          rhoncus nibh metus nec massa. Maecenas hendrerit laoreet augue nec
+          molestie. Cum sociis natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
+
+        <form>
+          <label>
+            Name:
+            <input type="text" name="name" />
+          </label>
+        </form>
+
+        <form>
+          <label>
+            UserID:
+            <input type="text" name="name" />
+          </label>
+        </form>
       </div>
+      
     );
   }
 }
- 
+
 export default Home;
--- my-bio/src/Main.js
+++ my-bio/src/Main.js
@@ -1,14 +1,59 @@
 import React, { Component } from "react";
 import { Route, NavLink, HashRouter } from "react-router-dom";
+
+//Adding firebase dependencies
+import { signInWithGoogle } from "./firebase/firebase.utils";
+import { auth } from "./firebase/firebase.utils";
+
 import Home from "./Home";
 import ResumeUpload from "./ResumeUpload";
 import Questions from "./Questions";
 import Contact from "./Contact";
 
 class Main extends Component {
+  //Creating a constructor
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
   render() {
     return (
       <HashRouter>
+        {/* SignIN with google */}
+        <div className='user-info'>
+        {
+
+          this.state.currentUser ?
+
+            (<div>
+              <div>Name: {this.state.currentUser.displayName}</div>
+              <div>Email: {this.state.currentUser.email}</div>
+
+              <button onClick={() => auth.signOut()}>LOG OUT</button>
+            </div>
+            ) :
+
+            <button onClick={signInWithGoogle}>SIGN IN WITH GOOGLE</button>
+
+        }
+      </div >
+        {/*links to our components*/}
         <div>
           <h1>My-BIO</h1>
           <ul className="header">
